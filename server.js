95809afb9158e7136f4053c515b232f319147574
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-09-01 16:13:54 -0700

    Created express backend

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Put API routes here, before the "catch all" route
+
+// The following "catch all" route is necessary for
+// a SPA's client-side routing to properly work
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+var port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
