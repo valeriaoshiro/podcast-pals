37639f614fc3a0e29aef8986ed0efb8bdc831523
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-09-05 18:01:34 -0700

    Add materialize, fixed key error

--- src/components/ShowPodcast/ShowPodcast.jsx
+++ src/components/ShowPodcast/ShowPodcast.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 // import $ from 'jquery';
+import {Card, CardTitle} from 'react-materialize';
 import './ShowPodcast.css';
 
 const ShowPodcast = (props) => {
     var allPodcasts = props.topPodcasts.map((podcast, index) => {
         return (
             <div className="col s2" key={index}>
-                <div className="card">
-                    <div className="card-image">
-                        <img src={podcast.artworkUrl600} alt={podcast.collectionName}/>
-                    </div>
-                    <div className="card-content">
-                        <p>{podcast.collectionName}</p>
-                    </div>
-                    <div className="card-action">
-                        <Link to={podcast.collectionViewUrl} target="_blank">Visit Podcast</Link>
-                    </div>
-                </div>
+                <Card 
+                    className='small'
+                    key={index}
+                    header={<CardTitle key={index} image='img/sample-1.jpg'>Card Title</CardTitle>}
+                    actions={[<a key={index} href='#'>This is a Link</a>]}>
+                    I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
+                </Card>
             </div>    
         )
            
