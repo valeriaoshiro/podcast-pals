8bfaab6db11f3eebc0d357595e5346c94100fef9
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-09-05 18:51:15 -0700

    Changed ShowPodcast to react-materilize

--- src/components/ShowPodcast/ShowPodcast.jsx
+++ src/components/ShowPodcast/ShowPodcast.jsx
@@ -1,33 +1,44 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-// import $ from 'jquery';
+import {Card, CardTitle, Row, Col} from 'react-materialize';
 import './ShowPodcast.css';
 
 const ShowPodcast = (props) => {
     var allPodcasts = props.topPodcasts.map((podcast, index) => {
         return (
-            <div className="col s2" key={index}>
-                <div className="card">
-                    <div className="card-image">
-                        <img src={podcast.artworkUrl600} alt={podcast.collectionName}/>
-                    </div>
-                    <div className="card-content">
-                        <p>{podcast.collectionName}</p>
-                    </div>
-                    <div className="card-action">
-                        <Link to={podcast.collectionViewUrl} target="_blank">Visit Podcast</Link>
-                    </div>
-                </div>
-            </div>    
+            <Col s={3} key={index}>
+                <Card
+                    key={index}
+                    className='small'
+                    header={<CardTitle key={index} image={podcast.artworkUrl600}></CardTitle>}
+                    actions={[<a href={podcast.collectionViewUrl} key={index}>Visit Podcast</a>]}>
+                    {podcast.collectionName}
+                </Card>
+
+            </Col>
+
+
+            // <div className="col s2" key={index}>
+            //     <div className="card">
+            //         <div className="card-image">
+            //             <img src={podcast.artworkUrl600} alt={podcast.collectionName}/>
+            //         </div>
+            //         <div className="card-content">
+            //             <p>{podcast.collectionName}</p>
+            //         </div>
+            //         <div className="card-action">
+            //             <Link to={podcast.collectionViewUrl} target="_blank">Visit Podcast</Link>
+            //         </div>
+            //     </div>
+            // </div>    
         )
            
     });
 
     return (
         <div className="container">
-            <div className="row">
+            <Row>
                 {allPodcasts}
-            </div>    
+            </Row>    
       </div>
     );
 
