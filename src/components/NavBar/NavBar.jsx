9b7398144685ddf6e51aa294c3e8f5dffe44996c
Valeria Oshiro <valeriaoshiro19@gmail.com>
2017-09-05 17:41:50 -0700

    resolved errors and warnings. removed materialize.js import

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
-import $ from 'jquery';
+// import $ from 'jquery';
 import './NavBar.css';
 
 class NavBar extends Component {
